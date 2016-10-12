55c09fa698515146ec1b2d55cfdda654a8fa5c50
arrase <arrase@gmail.com>
2016-10-12 02:34:01 +0200

    counter start value

--- app/src/main/java/llanes/ezquerro/juan/megadldcli/click_actions/onServerClick.java
+++ app/src/main/java/llanes/ezquerro/juan/megadldcli/click_actions/onServerClick.java
@@ -4,7 +4,7 @@ import android.content.Context;
 
 public abstract class onServerClick {
     public String URL;
-    protected Integer COUNTER;
+    protected Integer COUNTER = 0;
 
     public abstract void run(Integer id, String ip, Integer port, Context context);
 }
